6d14a87d0b5674f43810ee1ffd6f548890123679
JuanLuisAzofeifa <juanluisazofeifa@gmail.com>
2020-03-31 21:47:06 -0600

    Actualizacion Incidencias

---
+++ Controllers/IncidenciasController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PortalRegistroIncidencias.Models;
+using System.Data.Entity;
+
+namespace PortalRegistroIncidencias.Controllers
+{
+    public class IncidenciasController : Controller
+    {
+        private ModelincidenciaContainer db = new ModelincidenciaContainer();
+        // GET: Incidencias
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Crear([Bind(Include = "Id_Incidencia,Latitud,Longitud,Detalle,Tipo_Incidencia_Id,Empresa_Id,Estado_Incidencia_Id,Fotografia_Id,Provincia_Id,Canton_Id,Distrito_Id,Usuario_Id")] Incidencias incidencias)
+        {
+            if(ModelState.IsValid)
+            {
+                //db.incidencia.Add(incidencias);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(incidencias);
+        }
+
+
+    }
+}
---
+++ Models/Incidencias.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace PortalRegistroIncidencias.Models
+{
+    public class Incidencias
+    {
+        
+        public int Id_Incidencia { get; set; }
+        [Required]
+        public string Latitud { get; set; }
+        [Required]
+        public string Longitud { get; set; }
+        public string Detalle { get; set; }
+        public int Tipo_Incidencia_Id { get; set; }
+        public int Empresa_Id { get; set; }
+        public int Estado_Incidencia_Id { get; set; }
+        public int Fotografia_Id { get; set; }
+        public int Provincia_Id { get; set; }
+        public int Canton_Id { get; set; }
+        public int Distrito_Id { get; set; }
+        public int Usuario_Id { get; set; }
+
+    }
+}
